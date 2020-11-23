e77aae234a69a9ae3d6f62f6b86ccdb9396bc35f
Doanh Ho Ngoc <doanh.ho@orientsoftware.com>
2020-11-23 13:24:51 +0700

    wake up

---
+++ Wakeup/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Wakeup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(22, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(122, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 52);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Coffee";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Wakeup/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wakeup
+{
+    delegate string GetState();
+    delegate void MakeAction();
+    
+
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        GetState getState;
+        MakeAction makeAction;
+
+        string appState = "Waiting";       
+
+        int moveRange = 10;
+
+        Thread doWorkThread;        
+
+        public Form1()
+        {            
+            InitializeComponent();
+            initState();                        
+        }
+
+        private void initState()
+        {
+            makeAction += PreventSleep;
+            getState += () => this.appState;
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;            
+
+            this.ShowInTaskbar = false;
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            appState = "Running";
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+            this.notifyIcon.Text = "ON";
+
+            doWorkThread = new Thread(() =>
+            {
+                var currentState = this.Invoke(getState);
+
+                Console.WriteLine(currentState);
+
+                while ((string)currentState == "Running")
+                {
+                    Thread.Sleep(3000);                                       
+                    currentState = this.Invoke(getState);                    
+                    this.Invoke(makeAction);
+                }
+            });
+
+            doWorkThread.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            appState = "Waiting";
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+            this.notifyIcon.Text = "OFF";
+        }
+
+        void PreventSleep()
+        {
+            //refer to https://stackoverflow.com/questions/49045701/prevent-screen-from-sleeping-with-c-sharp            
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+        }
+    }
+}
